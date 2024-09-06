d27ef67b8f5e5cb08a5d497f46323da6e2f23da5
Francis Castillo <drakecastillo61@gmail.com>
2024-09-06 08:31:08 -0400

    Cree el Modelo

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicosId { get; set;  }
+        [Required(ErrorMessage = "El Campo Descri&oacute;n es obligatorio")]
+        public string? TecnicosName { get; set; }
+    }
+}
