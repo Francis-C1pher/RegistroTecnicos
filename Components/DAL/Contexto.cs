5f5c09e4c39fb4c0160a0ede6dbb49ba636c5862
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-01-17 01:37:47 -0400

    Cree el contexto dentro de la carpeta DAL

---
+++ Components/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Components.Models;
+
+namespace RegistroTecnicos.Components.DAL
+{
+    public class Contexto:DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
