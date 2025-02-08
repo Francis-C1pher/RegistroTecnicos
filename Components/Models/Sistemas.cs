80d8291abbef42cf3014239319babe970447d76a
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-02-07 23:44:34 -0400

    Reescribe la variable "Complegidad" por " Complejidad" asi que hice la nueva migracion y actualize la base de datos,

--- Components/Models/Sistemas.cs
+++ Components/Models/Sistemas.cs
@@ -12,6 +12,6 @@ namespace RegistroTecnicos.Components.Models
         public string Descripcion { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Este campo es requerido")]
-        public string Complegidad { get; set; } = string.Empty;
+        public string Complejidad { get; set; } = string.Empty;
     }
 }
