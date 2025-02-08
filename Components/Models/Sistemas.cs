ce3ceb7ee69d9f31da696b2cdaf4ffb690c3a79a
Francis-C1pher <francis_castillo4@ucne.edu.do>
2025-02-07 21:52:39 -0400

    Cree el modelo de Sistemas, lo agrege al contexto, cree la migracion y actualiza la base de datos.

---
+++ Components/Models/Sistemas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnicos.Components.Models
+
+{
+    public class Sistemas
+    {
+
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Complegidad { get; set; } = string.Empty;
+    }
+}
